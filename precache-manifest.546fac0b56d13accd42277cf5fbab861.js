ecf35f98742393277d16b9d7751a7a17f759698e
Lungnapa <rungnapa.09np@gmail.com>
2020-09-02 19:27:31 +0700

    Updates

---
+++ precache-manifest.546fac0b56d13accd42277cf5fbab861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de844cc7dc17e624d7d1333099efd93b",
+    "url": "/rungnapa/index.html"
+  },
+  {
+    "revision": "fcc273120648c11e3a87",
+    "url": "/rungnapa/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "07abef556aa3dfecdc99",
+    "url": "/rungnapa/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "fcc273120648c11e3a87",
+    "url": "/rungnapa/static/js/2.5cd1425c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rungnapa/static/js/2.5cd1425c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07abef556aa3dfecdc99",
+    "url": "/rungnapa/static/js/main.cc030c05.chunk.js"
+  },
+  {
+    "revision": "5d3dd26159aa8b54d65e",
+    "url": "/rungnapa/static/js/runtime-main.6621ca5d.js"
+  }
+]);
